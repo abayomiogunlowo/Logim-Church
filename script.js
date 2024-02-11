63f8e04e2ff56aba093f3cd47731b460a69249bd
Abayomi Ogunlowo <abayomiogunlowo@gmail.com>
2024-02-11 00:27:58 -0500

    OVERHAUL

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.10.2/fullcalendar.min.js"></script>
 
 var navLinks = document.getElementById("navLinks");
 
@@ -9,14 +8,3 @@ function showMenu() {
 function hideMenu() {
     navLinks.style.right = "-200px";
 }
-
-
-$(document).ready(function() {
-    $('#calendar').fullCalendar({
-        googleCalendarApiKey: 'AIzaSyDDmQkNENe4B3SBeGHHXP067X6vOdYQbGc',
-        events: {
-            googleCalendarId: '2dd0d4895fdc5b6c8c4deb2ddc83fd7c00870b0760b1b1648b8fd76da551c96a@group.calendar.google.com',
-        },
-    });
-});
-
